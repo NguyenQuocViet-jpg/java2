07c05f1b62206d50560e8bb668be92dd5dce9f87
Nguyen Quoc Viet <nguyenviet141125@gmail.com>
2026-03-17 14:49:35 +0700

    "upload code"

--- Lab1_48341/src/Bai1_QLSanPham/Main.java
+++ Lab1_48341/src/Bai1_QLSanPham/Main.java
@@ -1,30 +1,27 @@
 package Bai1_QLSanPham;
-//
-//import java.util.Scanner;
+
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		dsSanPham danhsach = new dsSanPham();
-//		
-//		do {
-//			danhsach.inputsp(sc);
-//			
-//			System.out.print("Bạn có muốn nhập loại sản phẩm khác không(Y/N)");
-//			String tiep = sc.nextLine().trim();
-//			if(tiep.equalsIgnoreCase("n"))
-//				break;
-//		}while(true);
-//		
-//		danhsach.printSp();
-//		
-//		danhsach.giaCaoI();
-//		
+		Scanner sc = new Scanner(System.in);
+		dsSanPham danhsach = new dsSanPham();
 		
-		Product moi = new Product("", "", -2);
+		do {
+			danhsach.inputsp(sc);
+			
+			System.out.print("Bạn có muốn nhập loại sản phẩm khác không(Y/N)");
+			String tiep = sc.nextLine().trim();
+			if(tiep.equalsIgnoreCase("n"))
+				break;
+		}while(true);
 		
-		System.out.println(moi);
+		danhsach.printSp();
+		
+		danhsach.giaCaoI();
+		
+		
 	}
 
 }
